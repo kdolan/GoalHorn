6f9dc8412d734b50aba293944e47304b8a8bf795
Kevin J Dolan <git@kevinjdolan.com>
2015-10-03 11:29:45 -0400

    Adds files

---
+++ constants.py
@@ -0,0 +1,13 @@
+#Pins
+GPIO_GOAL_LIGHT = 17
+GPIO_GOAL_BUTTON = 4
+GPIO_KEY = 27
+
+#Paths
+READY_STRING = "Action completed"
+DEFAULT_HORN = "rit_horn_short_band.wav"
+TRIGGER_PATH = "/horns/scripts/GOAL.trigger"
+READY_STRING = "Waiting for a goal..."
+AUDIO_ROOT = "/horns/"
+DEFAULT_GOAL_FILE = "rit_horn_short_band.wav"
+DEFAULT_PENALTY_FILE = "penalty.wav"
